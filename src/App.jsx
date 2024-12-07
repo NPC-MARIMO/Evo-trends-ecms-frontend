3616f296e6419f55dc0b8e8c2d7ee083f48a90d6
NPC-MARIMO <shivangbhaiisgreat@gmail.com>
2024-12-07 14:41:27 +0530

    navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import AuthLayout from "./components/auth/layout";
 import AuthLogin from "./pages/auth/login";
 import AuthRegister from "./pages/auth/register";
@@ -34,6 +34,12 @@ function App() {
   }, [dispatch]);
 
   if (isLoading) return <Skeleton className="w-[800] bg-black h-[600px]" />;
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    navigate("/")
+  }, [navigate])
 
   console.log(isLoading, user);
 
